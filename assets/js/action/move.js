66686b1b1d4dbae904b12698cfd1d7a75c69f7a5
Ubuntu <garrett.s.newman@gmail.com>
2018-02-11 00:53:47 +0000

    finally figured out dang prototypal inheritance using Object.create and no constructors

--- assets/js/action/move.js
+++ assets/js/action/move.js
@@ -1,10 +1,11 @@
-game.Action.Move = {
+game.Action.Move = Object.create(game.Action);
+
+Object.assign(game.Action.Move, {
   init : function(fromBlock, toBlock) {
-    //game.Action.init('sldkjlkj');
+    game.Action.init.call(this, 'move');
+
     this.fromBlock = fromBlock;
     this.toBlock = toBlock;
-
-    this.type = 'move';
   },
-};
+});
 
